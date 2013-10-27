958f493f6d3ca9fad3db63f08a178740a289e49d
Brian Moore <guardbionic-github@yahoo.com>
2013-10-27 12:00:08 -0400

    add error message to join template

--- client/js/controllers/joinCtrl.js
+++ client/js/controllers/joinCtrl.js
@@ -3,13 +3,17 @@
 module.exports = function joinCtrl($scope, $location, StackRes, Local) {
     $scope.name = "";
     $scope.passphrase = StackRes.stack.passphrase || "";
+    $scope.errorMessage = "";
 
     $scope.join = function () {
-        if ($scope.name && $scope.passphrase && StackRes.stack.title) {
+        if ($scope.name && $scope.passphrase) {
+            $scope.errorMessage = "";
             StackRes.join($scope.name).then(function () {
                 Local.create(StackRes.stack.title, $scope.passphrase, $scope.name);
                 $location.path("/stack");
             });
+        } else {
+            $scope.errorMessage = "Please provide a valid name and passphrase.";
         }
     };
 };
