c12cf5c78ff0c1ecc46450255588f3d97d0d1b01
Brian Moore <guardbionic-github@yahoo.com>
2013-11-03 15:17:28 -0500

    clean up status display for each member

--- client/js/controllers/stackCtrl.js
+++ client/js/controllers/stackCtrl.js
@@ -1,20 +1,27 @@
 "use strict";
 
+var _ = require("underscore");
+
 module.exports = function stackCtrl($scope, StackRes, TimerRes) {
     var GRACE = 10;
     $scope.stack = StackRes.stack;
 
-    function count(timeLeft) {
-        console.log("WE ONLY HAVE %s SECONDS LEFT", timeLeft);
-        $scope.timeLeft = timeLeft;
+    function safeApply() {
         if (!$scope.$$phase) {
             $scope.$apply();
         }
     }
 
+    function count(timeLeft) {
+        console.log("WE ONLY HAVE %s SECONDS LEFT", timeLeft);
+        $scope.timeLeft = timeLeft;
+        safeApply();
+    }
+
     function done() {
         console.log("we are done");
         $scope.checkOut();
+        safeApply();
     }
 
     TimerRes.init(GRACE, count, done);
@@ -26,10 +33,14 @@ module.exports = function stackCtrl($scope, StackRes, TimerRes) {
         });
     };
 
-    $scope.checkOut = StackRes.checkOut;
+    $scope.checkOut = function () {
+        StackRes.checkOut().then(safeApply);
+    };
 
-    $scope.isInactive = function () {
-        return !StackRes.currentMember.inStack;
+    $scope.isInactive = function (name) {
+        var memberName = name || StackRes.currentMember.name;
+        var member = _.findWhere(StackRes.stack.members, { name: memberName });
+        return !member.inStack;
     };
 
     $scope.isCountingDown = function () {
